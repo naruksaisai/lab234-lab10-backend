ae8f3cbfc0cf1f0350618d7bc0df00ddef8723a1
Unknown <joniejostar@gmail.com>
2018-03-29 20:10:54 +0700

    Grade#1

--- src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/lab10/service/GradeServiceImpl.java
@@ -6,8 +6,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class GradeServiceImpl implements GradeService {
     @Override
-    public String getGrade(double score) {  
-            return "F";
+    public String getGrade(double score) {
+        if (score >= 80) {
+            return "A";
+        } else if (score >= 75 && score < 80) {
+            return "B";
+        } else if (score >= 60 && score < 75) {
+            return "C";
+        } else if (score >= 33 && score < 60) {
+            return "D";
+        }
+        return "F";
 
     }
 }
